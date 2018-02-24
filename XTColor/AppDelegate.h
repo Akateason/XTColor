880266e49ca4adfba61e50660e73a5988e31b195
teason <teason.xie@cootek.cn>
2018-02-24 18:29:26 +0800

    Initial Commit

---
+++ XTColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTColor
+//
+//  Created by teason23 on 2018/2/24.
+//  Copyright © 2018年 teason23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
