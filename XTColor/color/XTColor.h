a00a9e90588c8422c54afb1c6964dc753ef76a54
谢天宸 <xietianchen@shimo.im>
2019-01-09 13:53:22 +0800

    debug

--- XTColor/color/XTColor.h
+++ XTColor/color/XTColor.h
@@ -12,16 +12,11 @@
 #import "UIColor+XTAddition.h"
 
 
-#undef  UIColorRGBA
 #define UIColorRGBA(r, g, b, a)         [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
-
-#undef  UIColorRGB
 #define UIColorRGB(r, g, b)             UIColorRGBA(r, g, b, 1.0)
 
-#undef  UIColorHex
-#define UIColorHex(X)                   [UIColor colorWithHexString:X]
 
-#undef  UIColorHexA
+#define UIColorHex(X)                   [UIColor colorWithHexString:X]
 #define UIColorHexA(X,a)                [UIColor colorWithHexString:X alpha:a]
 
 
